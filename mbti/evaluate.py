308485c83c1c4cd88773bc5ddef81303ab5e0cbb
Nan YE <dailystudio2010@gmail.com>
2018-12-17 10:00:39 +0800

    [mbti][DEV]: remove useless codes;

--- mbti/evaluate.py
+++ mbti/evaluate.py
@@ -1,100 +1,12 @@
 import os
 import argparse
-import timeit
 
 import numpy as np
 import data_process
 import tensorflow as tf
 from tensorflow import keras
 
-DEFAULT_EPOCHS = 40
-DEFAULT_BATCH_SIZE = 64
-DEFAULT_TRAIN_FRACTION = .8
-DEFAULT_EVAL_FRACTION = .2
-DEFAULT_EMBEDDING_DIM = 300
-
 MODEL_FILE_NAME_TEMPLATE = 'model_{}_seq_{}_epoch_{}_embedding_{}.h5'
-
-
-def train_category(category,
-                   train_x, train_y, test_x, test_y,
-                   embedding_layers,
-                   eval_split,
-                   epoch, batch_size):
-    start = timeit.default_timer()
-
-    model = keras.Sequential()
-    model.add(embedding_layers)
-
-    # model.add(keras.layers.GlobalAveragePooling1D())
-    model.add(keras.layers.Bidirectional(keras.layers.LSTM(64)))
-    # model.add(keras.layers.Dense(16, activation=tf.nn.relu))
-    model.add(keras.layers.Dense(1, activation=tf.nn.sigmoid))
-
-    model.summary()
-
-    model.compile(optimizer=tf.train.AdamOptimizer(),
-                  loss='binary_crossentropy',
-                  metrics=['accuracy'])
-
-    history = model.fit(train_x,
-                        train_y,
-                        epochs=epoch,
-                        batch_size=batch_size,
-                        validation_split=eval_split,
-                        shuffle=True,
-                        verbose=1)
-
-    results = model.evaluate(test_x, test_y)
-
-    outputs = model.predict(test_x)
-    predicted = np.round(outputs).reshape(len(outputs)).astype(int)
-    print('MODEL: {}, outputs: {}'.format(category, outputs))
-    print('MODEL: {}, predicted: {}'.format(category, predicted))
-
-    end = timeit.default_timer()
-
-    print('MODEL: {} is trained in {}s ({}s per epoch), results: {}'.format(
-        category, round(end - start), round((end - start) / epoch), results))
-
-    return model
-
-
-def train_by_columns(data_file, voc_file, model_dir, glove_dir,
-                     epoch, batch_size,
-                     train_split, eval_split,
-                     embedding_dim):
-    print('training: data = {}, voc = {}, '
-          'epochs = {}, batch size = {}, '
-          'glove dir = {}, embedding dim = {} ---> models = {}'.format(data_file, voc_file,
-                                                                       epoch, batch_size,
-                                                                       glove_dir, embedding_dim,
-                                                                       model_dir))
-
-    train_x, train_y, test_x, test_y = data_process.load_data(
-        data_file, train_split)
-    vocab = data_process.load_vocab(voc_file)
-
-    max_seq = train_x.shape[1]
-    vocab_size = len(vocab) + 1
-    print('training: sequence length = {}, vocabulary size = {}'.format(
-        max_seq, vocab_size))
-
-    embedding_layers = data_process.load_embeddings(
-        vocab, max_seq, glove_dir, embedding_dim)
-
-    for i in range(0, 4):
-        model_name = 'model_{}'.format(i)
-        model = train_category(model_name,
-                               train_x, train_y.iloc[:, i],
-                               test_x, test_y.iloc[:, i],
-                               embedding_layers,
-                               eval_split,
-                               epoch, batch_size)
-
-        model.save(os.path.join(model_dir,
-                                MODEL_FILE_NAME_TEMPLATE.format(i, max_seq,
-                                                                epoch, embedding_dim)))
 
 
 def predict_file(input_file, model_dir, model_prefix):
